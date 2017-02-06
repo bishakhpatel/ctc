88617b4d5dbce64ee26e36fdb2dec0f9a0687e14
Aparna Kumari <akumari1@levi.com>
2017-02-06 17:20:05 +0530

    First Commit in CTC

---
+++ ctc/ctccockpits/src/com/ctc/cockpits/constants/CtcCockpitsConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.ctc.cockpits.constants;
+
+/**
+ * Global class for all CtcCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class CtcCockpitsConstants extends GeneratedCtcCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "ctccockpits";
+
+	public static final String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private CtcCockpitsConstants()
+	{
+		// empty
+	}
+}
---
+++ ctc/ctcfacades/gensrc/com/ctc/facades/constants/GeneratedCtcFacadesConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 6, 2017 5:01:18 PM                      ---
+ * ----------------------------------------------------------------
+ */
+package com.ctc.facades.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCtcFacadesConstants
+{
+	public static final String EXTENSIONNAME = "ctcfacades";
+	
+	protected GeneratedCtcFacadesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
